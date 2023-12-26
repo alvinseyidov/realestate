048eaefcd97d914e7141f4879794f603f8ae263d
Elvin Seyidov <alvinseyidov@gmail.com>
2023-12-26 15:30:57 +0400

    update

--- offer/views.py
+++ offer/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from core.models import General, Social, Why, Tablar
+from core.models import General, Social, Why, Tablar, Head, Body
 from offer.models import Offer
 
 
@@ -10,7 +10,16 @@ def offer(request, id):
     tablar = Tablar.objects.all()
     offer = Offer.objects.get(pk=id)
     offers = Offer.objects.all()
+    if request.user_agent.is_mobile:
+        is_mobile = True
+    else:
+        is_mobile = False
+    head = Head.objects.all()
+    body = Body.objects.all()
     context = {
+        'body': body,
+        'head': head,
+        'is_mobile': is_mobile,
         "offers": offers,
         "offer": offer,
         "tablar": tablar,
